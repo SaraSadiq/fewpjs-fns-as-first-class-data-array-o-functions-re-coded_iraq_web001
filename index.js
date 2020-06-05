dcd95f957d6dee7d4334f2f80d3acbc452333b6f
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-06-05 17:52:50 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+
+  function wakeDog() {
+    let routine=['Xerox','Border Collie']
+    console.log()
+
+}
+
+function leashDog() {
+  console.log("Leash Byron the poodle");
+}
+function walkToPark() {
+  console.log("Walk to the park with Byron the poodle");
+}
+function throwFrisbee() {
+  console.log("Throw the frisbee for Byron the poodle");
+}
+function walkHome() {
+  console.log("Walk home with Byron the poodle");
+}
+function unleashDog() {
+  console.log("Unleash Byron the poodle");
+}
+
+function exerciseDog(dogName,dogBreed) {
+
+}
